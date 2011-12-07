8576aaeb2c19e2486066644e39a0eab7be8929d0
Philip Cali <philip.cali@gmail.com>
2011-12-07 14:25:59 -0600

    Upgrade script for old manual items

--- grade/report/grader/version.php
+++ grade/report/grader/version.php
@@ -24,6 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012061700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012072300;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012061700;        // Requires this Moodle version
 $plugin->component = 'gradereport_grader'; // Full name of the plugin (used for diagnostics)
+
