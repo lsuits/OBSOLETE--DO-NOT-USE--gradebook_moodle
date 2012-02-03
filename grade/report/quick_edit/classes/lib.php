9c594c7e613b8fd1cb9898312df74d3b09d28022
Philip Cali <philip.cali@gmail.com>
2012-02-03 12:25:10 -0600

    Grade screen and grade selector.

--- grade/report/quick_edit/classes/lib.php
+++ grade/report/quick_edit/classes/lib.php
@@ -19,6 +19,67 @@ abstract class quick_edit_screen {
         $this->init();
     }
 
+    public function format_range($item) {
+        $decimals = $item->get_decimals();
+
+        $min = format_float($item->grademin, $decimals);
+        $max = format_float($item->grademax, $decimals);
+
+        return "$min - $max";
+    }
+
+    public function format_grade($grade, $decimals) {
+        $name = 'grade_' . $grade->itemid . '_' . $grade->userid;
+
+        $finalgrade = $grade->finalgrade ?
+            format_float($grade->finalgrade, $decimals) :
+            '';
+
+        $attributes = array(
+            'type' => 'text',
+            'name' => $name,
+            'value' => $finalgrade
+        );
+
+        $hidden = array(
+            'type' => 'hidden',
+            'name' => 'old' . $name,
+            'value' => $finalgrade
+        );
+
+        return (
+            html_writer::empty_tag('input', $attributes) .
+            html_writer::empty_tag('input', $hidden)
+        );
+    }
+
+    public function format_feedback($grade) {
+        $name = 'feedback_' . $grade->itemid . '_' . $grade->userid;
+
+        $feedback = $grade->feedback ?
+            format_text($grade->feedback, $grade->feedbackformat) :
+            '';
+
+        $attributes = array(
+            'type' => 'text',
+            'name' => $name,
+            'value' => $feedback
+        );
+
+        $hidden = array(
+            'type' => 'hidden',
+            'name' => 'old' . $name,
+            'value' => $feedback
+        );
+
+        return (
+            html_writer::empty_tag('input', $attributes) .
+            html_writer::empty_tag('input', $hidden)
+        );
+    }
+
+    public abstract function heading();
+
     public abstract function init();
 
     public abstract function html();
---
+++ grade/report/quick_edit/screens/select/lib.php
@@ -0,0 +1,36 @@
+<?php
+
+// TODO: custom form with group selector
+class quick_edit_select extends quick_edit_screen {
+    public function init() {
+        global $DB;
+
+        $params = array('courseid' => $this->courseid);
+
+        $filter_items = function($item) {
+            return $item->itemtype != 'course' and $item->itemtype != 'category';
+        };
+
+        $this->grade_items = array_filter(grade_item::fetch_all($params), $filter_items);
+
+        $this->item = $DB->get_record('course', array('id' => $this->courseid));
+    }
+
+    public function html() {
+        global $OUTPUT;
+
+        $map = function($item) { return $item->itemname; };
+
+        $grade_options = array_map($map, $this->grade_items);
+
+        $params = array(
+            'id' => $this->courseid,
+            'item' => 'grade',
+            'group' => $this->groupid
+        );
+
+        $url = new moodle_url('/grade/report/quick_edit/index.php', $params);
+
+        echo $OUTPUT->single_select($url, 'itemid', $grade_options, $this->itemid);
+    }
+}
