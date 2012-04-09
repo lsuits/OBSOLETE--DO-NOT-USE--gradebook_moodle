3f26093811baee1c9cc13ea04ed2781852102546
Philip Cali <philip.cali@gmail.com>
2012-04-09 15:20:19 -0500

    Anonymous grade temporary grading.

--- lib/grade/grade_anonymous.php
+++ lib/grade/grade_anonymous.php
@@ -33,8 +33,81 @@ class grade_anonymous extends grade_object {
         return $this->grade_item;
     }
 
+    public function load_grade($user, $default=true) {
+        if (empty($this->itemid) or empty($this->id)) {
+            return array();
+        }
+
+        $grade = grade_anonymous_grade::fetch(array(
+            'anonymous_itemid' => $this->id,
+            'userid' => $user->id
+        ));
+
+        if (!$grade and $default) {
+            $instance = new stdClass;
+
+            $instance->anonymous_itemid = $this->id;
+            $instance->userid = $user->id;
+
+            $grade = new grade_anonymous_grade($instance, false);
+        }
+
+        if ($grade) {
+            // TODO: rethink db... rawgrade plus itemid?
+            $grade->anonymous_item = $this;
+            $grade->grade_item = $this->load_item();
+            $grade->itemid = $grade->grade_item->id;
+            $grade->rawgrade = $grade->finalgrade;
+        }
+
+        return $grade;
+    }
+
+    public function check_completed($real_users) {
+        $anon_users = $this->anonymous_users($real_users);
+
+        $real_count = count($real_users);
+
+        if (count($anon_users) != $real_count) {
+            return false;
+        }
+
+        global $DB;
+
+        $userids = implode(',', array_keys($real_users));
+        $select = 'userid IN (' . $userids.') AND anonymous_itemid = :itemid';
+        $params = array('itemid' => $this->id);
+
+        $count = $DB->count_records_select('grade_anon_grades', $select, $params);
+
+        return $real_count == $count;
+    }
+
     public function is_completed() {
         return $this->complete;
+    }
+
+    public function set_completed($status = true) {
+        $this->complete = $status;
+        $this->update();
+
+        if ($this->complete) {
+            $grades = grade_anonymous_grade::fetch_all(array(
+                'anonymous_itemid' => $this->id
+            ));
+
+            foreach ($grades as $grade) {
+                $this->load_item()->update_final_grade(
+                    $grade->userid, $grade->real_grade()
+                );
+            }
+        } else {
+            $grades = grade_grade::fetch_all(array('itemid' => $this->itemid));
+
+            foreach ($grades as $grade) {
+                $grade->delete();
+            }
+        }
     }
 
     public static function anonymous_profile() {
@@ -68,7 +141,7 @@ class grade_anonymous extends grade_object {
 
         $userids = implode(',', array_keys($real_users));
 
-        $sql = 'SELECT d.userid, d.data FROM {user_info_data} d
+        $sql = 'SELECT d.userid AS id, d.data FROM {user_info_data} d
             WHERE d.userid IN (' . $userids.')
               AND d.fieldid = :fieldid';
 
@@ -95,5 +168,62 @@ class grade_anonymous extends grade_object {
         }
 
         return false;
+    }
+}
+
+class grade_anonymous_grade extends grade_object {
+    public $table = 'grade_anon_grades';
+
+    var $required_fields = array(
+        'id', 'userid', 'anonymous_itemid', 'finalgrade', 'adjust_value'
+    );
+
+    var $anonymous_itemid;
+
+    var $userid;
+
+    var $finalgrade;
+
+    var $adjust_value = 0.00000;
+
+    var $anonymous_item;
+
+    var $itemid;
+
+    var $rawgrade;
+
+    var $grade_item;
+
+    public static function fetch($params) {
+        return grade_object::fetch_helper(
+            'grade_anon_grades', 'grade_anonymous_grade', $params
+        );
+    }
+
+    public static function fetch_all($params) {
+        return grade_object::fetch_all_helper(
+            'grade_anon_grades', 'grade_anonymous_grade', $params
+        );
+    }
+
+    public function load_item() {
+        if (empty($this->anonymous_item)) {
+            $params = array('id' => $this->anonymous_itemid);
+            $this->anonymous_item = grade_anonymous::fetch($params);
+        }
+
+        return $this->anonymous_item;
+    }
+
+    public function load_grade_item() {
+        if (empty($this->grade_item)) {
+            $this->grade_item = $this->load_item()->load_item();
+        }
+
+        return $this->grade_item;
+    }
+
+    public function real_grade() {
+        return $this->finalgrade + (float)$this->adjust_value;
     }
 }
