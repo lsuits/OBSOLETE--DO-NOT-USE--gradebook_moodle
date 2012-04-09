0feb5df32e897cc661818c038d83fe8524ea19d4
Philip Cali <philip.cali@gmail.com>
2012-04-09 08:38:13 -0500

    Preliminary library work

---
+++ lib/grade/grade_anonymous.php
@@ -0,0 +1,61 @@
+<?php
+
+class grade_anonymous extends grade_object {
+    var $required_fields = array('id', 'itemid', 'complete');
+
+    var $id;
+
+    var $itemid;
+
+    var $complete = false;
+
+    var $grade_item;
+
+    public $table = 'grade_anonymous_items';
+
+    public static function fetch($params) {
+        return grade_object::fetch_helper(
+            $this->table, 'grade_anonymous', $params
+        );
+    }
+
+    public static function fetch_all($params) {
+        return grade_object::fetch_all_helper(
+            $this->table, 'grade_anonymous', $params
+        );
+    }
+
+    public function load_item() {
+        if (empty($this->grade_item) and !empty($this->itemid)) {
+            $this->grade_item = grade_item::fetch(array('id' => $this->itemid));
+        }
+
+        return $this->grade_item;
+    }
+
+    public function check_completed($course) {
+    }
+
+    public function is_completed() {
+        return $this->complete;
+    }
+
+    public static function is_supported($course) {
+        // Enabled system wide?
+        $enabled = (bool)get_config('moodle', 'grade_anonymous_grading');
+
+        $cats = explode(',', get_config('moodle', 'grade_anonymous_cats'));
+
+        $is_cat = (empty($cats) or in_array($course->category, $cats));
+
+        return ($enabled and $is_cat);
+    }
+
+    public function delete($source = null) {
+        if (parent::delete($source)) {
+            return $this->load_item()->delete($source);
+        }
+
+        return false;
+    }
+}
